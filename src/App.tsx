755a8c36a7a5ddfae0bca566325c5a748aca1319
Marcus <marcus.rosland.goplen@entur.org>
2020-08-13 17:05:09 +0200

    Version 1

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import Home from "./Pages/Home";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <Home />
+  );
+}
+
+export default App;
